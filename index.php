020062cfbff40b0f8d67282f9a17134567c48040
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-15 15:14:58 -0600

    Setting up form and submit on webpage

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+
+
+
+?>
+<hr>
+
+<form action="index.php" method="POST">
+   <textarea name="user_input" rows="6" cols="30"></textarea><br><br>
+   <input type="submit" value="Submit">
+</form>
